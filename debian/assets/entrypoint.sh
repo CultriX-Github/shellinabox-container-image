c730c787e7a1dbcce8884fe64d8e2926d663d30b
CultriX <57349729+CultriX-Github@users.noreply.github.com>
2025-07-20 12:12:49 +0200

    Make shell default

--- debian/assets/entrypoint.sh
+++ debian/assets/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e  # Exit immediately on error
 
-# Define the default command
-COMMAND="shellinabox"
+# Define the default command (interactive shell)
+COMMAND="${SIAB_SHELL:-/bin/bash}"
 
 # Check if a startup script is provided and not set to "none"
 if [ -n "$SIAB_SCRIPT" ] && [ "$SIAB_SCRIPT" != "none" ]; then
@@ -28,10 +28,10 @@ fi
 
 echo "Starting container..."
 
-# If no arguments are provided, default to shellinabox
+# If no arguments are provided, start an interactive shell
 if [ $# -eq 0 ]; then
-    echo "No arguments provided, defaulting to: $COMMAND"
-    exec $COMMAND
+    echo "No arguments provided, launching shell: $COMMAND"
+    exec "$COMMAND" -l
 else
     echo "Executing provided command: $@"
     exec "$@"
